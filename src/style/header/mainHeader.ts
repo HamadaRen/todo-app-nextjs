bb84b6ad75cf4eec1802eeeb24ed16fac0f81028
HamadaRen <hamada@two-six.jp>
2025-05-13 15:12:17 +0900

    feat/ スタイル作成

---
+++ src/style/header/mainHeader.ts
@@ -0,0 +1,28 @@
+'use client';
+import styled from 'styled-components';
+
+export const MainHeaderContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: #005243;
+  display: grid;
+  grid-template-columns: 10% 1fr;
+  font-weight: 700;
+  font-size: 2rem;
+`;
+
+export const HeaderItemIcon = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+align-items: center;
+padding-left: 5rem;
+`
+
+export const HeaderItemTitle = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+align-items: center;
+justify-content: center;
+`
